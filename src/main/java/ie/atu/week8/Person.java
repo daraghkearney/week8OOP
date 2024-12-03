6bde9878437944d519472c49566780921b95a74b
daraghkearney <g00438215@atu.ie>
2024-12-03 13:51:01 +0000

    added TestPreferedCustomer

--- src/main/java/ie/atu/week8/Person.java
+++ src/main/java/ie/atu/week8/Person.java
@@ -12,20 +12,44 @@ public class Person {
         this.phone = "";
     }
 
-
     public Person(String name, String address, String phone) {
         this.name = name;
         this.address = address;
         this.phone = phone;
     }
 
+    // Getter and Setter for Name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for Address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Getter and Setter for Phone Number
+    public String getPhoneNumber() {
+        return phone;
+    }
+
+    public void setPhoneNumber(String phone) {
+        this.phone = phone;
+    }
+
     @Override
     public String toString() {
-        return
-                "name='" + name + '\'' +
+        return "name='" + name + '\'' +
                 ", address='" + address + '\'' +
                 ", phone='" + phone + '\'' +
                 '}';
     }
 }
-
